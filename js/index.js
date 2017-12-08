8bc2198c80a5da7dddb871dbe17cac06c74eda32
Trung <trunglive@gmail.com>
2017-12-07 23:02:58 -0500

    revision #2

--- js/index.js
+++ js/index.js
@@ -1,33 +1,39 @@
-$(document).ready(function() {
-  $('form').on('submit', function(event) {
-
+const main = () => {
+  $('.wiki-search').on('submit', event => {
     event.preventDefault();
-
     $('.result-list').empty();
-    const data = $('.wiki-search__input').val();
-    const url = `https://en.wikipedia.org/w/api.php?action=query&srsearch=${data}&list=search&format=json`;
+
+    const input = $('.wiki-search__input').val();
+    const url = `https://en.wikipedia.org/w/api.php?action=query&srsearch=${input}&list=search&format=json`;
     
     $.ajax({
       type: 'GET',
       url: url,
       dataType: 'jsonp'
     })
-    .done(function(result) {
+    .done(result => {
       const articles = result.query.search;
       let htmlList = ``;
       
       for (let article of articles) {
-        let link = 'https://en.wikipedia.org/?curid=';
-        let finalLink = link + article.pageid;
-        
-        htmlList += `<li class='result-list__article'><a href=${finalLink} target="_blank"><p><strong>${article.title}</strong></p><p>${article.snippet}</p></a></li>`;
+        const link = `https://en.wikipedia.org/?curid=${article.pageid}`;
+        htmlList +=
+         `<li class='result-list__article'>
+            <a href=${link} target='_blank'>
+              <p><strong>${article.title}</strong></p>
+              <p>${article.snippet}</p>
+            </a>
+          </li>`;
       }
       
-      $('.result-list').hide().append(htmlList).fadeIn(400);
+      $('.result-list').hide().append(htmlList).fadeIn(700);
       $('.wiki-search__input').val('');
     })
-    .fail(function() {
-      console.log('Error!!');
+    .fail(() => {
+      $('.wiki-search__input').val('');
+      $('.result-list').append(`<li class='result-list__error'>Please check your internet connection again!</li>`);
     })
   });
-});
+};
+
+$(document).ready(main);
